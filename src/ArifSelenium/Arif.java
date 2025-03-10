b2625fa44777d73b1e2dfbc5dafdb41908054e73
ArfEtl <etili9@hotmail.com>
2025-03-10 23:21:44 +0300

    new sentences

--- src/ArifSelenium/Arif.java
+++ src/ArifSelenium/Arif.java
@@ -3,5 +3,9 @@ package ArifSelenium;
 public class Arif {
     public static void main(String[] args) {
         System.out.println("args = " + args);
+
+        System.out.println("arif merhaba");
     }
+
+
 }
